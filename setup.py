598ba22ea231bb2b8be04ae28a7c136f0b30a9fd
Danilo Vargas <danilovargas.01@gmail.com>
2020-02-26 20:20:42 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+install_requires = [
+    'motor==2.1.0',
+    'pymongo==3.10.1'
+]
+
+
+test_require = [
+    'pytest==5.3.5',
+    'pytest-asyncio==0.10.0',
+    'ipython==7.12.0'
+
+]
+
+setup(
+    name='meliodas',
+    version='1.0.0',
+    packages=['meliodas'],
+    install_requires=install_requires,
+    author='Danilo Vargas',
+)
