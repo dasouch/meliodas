058e4a4fd9d2363f43b64caa6a9f35e1e2cb96da
Danilo Vargas <danilovargas.01@gmail.com>
2020-03-22 16:28:25 -0500

    Update version 1.0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_require = [
 
 setup(
     name='meliodas',
-    version='1.0.5',
+    version='1.0.6',
     packages=['meliodas'],
     install_requires=install_requires,
     author='Danilo Vargas',
