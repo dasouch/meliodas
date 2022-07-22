ffc79dbea0f5d0a081959302ce0b38e9661cfb9f
Danilo Vargas <danilovargas.01@gmail.com>
2022-07-21 23:41:00 -0500

    Fixed bug in connection

--- meliodas/connection.py
+++ meliodas/connection.py
@@ -1,34 +1,23 @@
 import asyncio
-from threading import Lock
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from meliodas.settings import DB_USER, DB_PASSWORD, DB_SSL, DB_HOST, DB_PORT, DB_CA_CERTS
 
 
-class ConnectionMeta(type):
-
-    _instances = {}
-
-    _lock: Lock = Lock()
-
-    def __call__(cls, *args, **kwargs):
-        with cls._lock:
-            if cls not in cls._instances:
-                instance = super().__call__(*args, **kwargs)
-                cls._instances[cls] = instance
-        return cls._instances[cls]
-
-
-class Connection(metaclass=ConnectionMeta):
+class Connection:
     client: str = None
+    _instance = None
 
-    def __init__(self) -> None:
-        if DB_USER and DB_PASSWORD and DB_SSL:
-            self.client = AsyncIOMotorClient(
-                f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/?ssl=true&ssl_ca_certs={DB_CA_CERTS}&retryWrites=false')
-        elif DB_USER and DB_PASSWORD:
-            self.client = AsyncIOMotorClient(f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}')
-        else:
-            self.client = AsyncIOMotorClient(f'mongodb://{DB_HOST}:{DB_PORT}')
-        self.client.get_io_loop = asyncio.get_event_loop
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            if DB_USER and DB_PASSWORD and DB_SSL:
+                cls.client = AsyncIOMotorClient(
+                    f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/?ssl=true&tlsCAFile={DB_CA_CERTS}&retryWrites=false')
+            elif DB_USER and DB_PASSWORD:
+                cls.client = AsyncIOMotorClient(f'mongodb://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}')
+            else:
+                cls.client = AsyncIOMotorClient(f'mongodb://{DB_HOST}:{DB_PORT}')
+            cls.client.get_io_loop = asyncio.get_event_loop
+            cls._instance = object.__new__(cls)
+        return cls._instance
--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ test_require = [
 
 setup(
     name='meliodas',
-    version='1.1.4',
+    version='1.1.5',
     packages=['meliodas'],
     install_requires=install_requires,
     author='Danilo Vargas',
